669305310345ccc6177e146619a76bc709c2b6e3
adesha <adeshaholagodu@gmail.com>
2023-10-05 21:46:34 +0530

    linkedlist

---
+++ AlgoPrep/27SingleNumber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        vector<int>ans;
+        for(auto x:m){
+            if(x.second==1){
+                ans.push_back(x.first);
+            }
+        }
+        return ans;
+    }
+};
